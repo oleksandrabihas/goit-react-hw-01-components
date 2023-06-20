f1f5243cff10a71af6d273ce0c67f1de65a4e6a1
Oleksandra <sashka.bigas@gmail.com>
2023-06-20 14:58:49 +0200

    solved 2 tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import user from '../user.json';
+import data from '../data.json';
+// import friends from '../friends.json';
+import { Profile } from './Profile';
+import { Statistics } from './Statistics';
+// import { FriendList } from './FriendList';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile user={user} />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
+      {/* <FriendList friends={friends} /> */}
+    </>
   );
 };
